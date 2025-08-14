88e16d25af1098e947b4f57460f5eb412ae0fd28
sokolgj19 <gjekasokol19@gmail.com>
2025-08-14 11:32:29 +0000

    The gates of shell completed succesfully

--- main.c
+++ main.c
@@ -1,30 +1,48 @@
 #include "shell.h"
 
 /**
- * main - Entry point for the shell program
- * @argc: Argument count (unused)
- * @argv: Argument vector (program name and args)
- *
- * Return: Last command status
+ * handle_exit - handles the "exit" builtin
+ * @args: argument vector
+ * @line: input line
+ * @last_status: last command status
  */
-int main(int argc, char **argv)
+void handle_exit(char **args, char *line, int last_status)
 {
-	char *line;
-	size_t n;
-	ssize_t r;
-	int last_status;
-	int interactive;
+	free_argv(args);
+	free(line);
+	exit(last_status);
+}
 
-	(void)argc;
-	line = NULL;
-	n = 0;
-	last_status = 0;
-	interactive = isatty(STDIN_FILENO);
+/**
+ * handle_env - handles the "env" builtin
+ */
+void handle_env(void)
+{
+	int i;
+
+	for (i = 0; environ && environ[i]; i++)
+	{
+		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+	}
+}
+
+/**
+ * main_loop - runs the main shell loop
+ * @line: input buffer
+ * @n: buffer size
+ * @argv: program arguments
+ * @last_status: last command status pointer
+ * @interactive: interactive mode flag
+ */
+void main_loop(char *line, size_t n, char **argv,
+	       int *last_status, int interactive)
+{
+	ssize_t r;
+	char **args;
 
 	while (1)
 	{
-		char **args;
-
 		if (interactive)
 			write(STDOUT_FILENO, "($) ", 4);
 
@@ -37,40 +55,41 @@ int main(int argc, char **argv)
 		}
 
 		args = split_line(line);
-		if (!args)
-			continue;
-
-		if (!args[0])
+		if (!args || !args[0])
 		{
 			free_argv(args);
 			continue;
 		}
 
 		if (strcmp(args[0], "exit") == 0)
-		{
-			free_argv(args);
-			free(line);
-			exit(last_status);
-		}
+			handle_exit(args, line, *last_status);
 		else if (strcmp(args[0], "env") == 0)
-		{
-			int i;
-
-			for (i = 0; environ && environ[i]; i++)
-			{
-				write(STDOUT_FILENO, environ[i],
-				     strlen(environ[i]));
-				write(STDOUT_FILENO, "\n", 1);
-			}
-			free_argv(args);
-			continue;
-		}
-
-		execute_command(args, environ,
-				argv[0] ? argv[0] : "hsh", &last_status);
+			handle_env();
+		else
+			execute_command(args, environ,
+				argv[0] ? argv[0] : "hsh", last_status);
 
 		free_argv(args);
 	}
+}
+
+/**
+ * main - entry point of shell
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: last status
+ */
+int main(int argc, char **argv)
+{
+	char *line = NULL;
+	size_t n = 0;
+	int last_status = 0;
+	int interactive = isatty(STDIN_FILENO);
+
+	(void)argc;
+
+	main_loop(line, n, argv, &last_status, interactive);
 
 	free(line);
 	return (last_status);
